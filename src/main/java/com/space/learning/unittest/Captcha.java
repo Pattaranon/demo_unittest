e8ff9adce0427c34f88e4e79c0aefa93f39fdd4f
Pattaranon Plyduang <pattaranon.ply@gmail.com>
2021-06-11 14:22:10 +0700

    Add test case of captcha

--- src/main/java/com/space/learning/unittest/Captcha.java
+++ src/main/java/com/space/learning/unittest/Captcha.java
@@ -1,2 +1,13 @@
-package com.space.learning.unittest;public class Captcha {
+package com.space.learning.unittest;
+
+import org.apache.tomcat.util.bcel.Const;
+
+public class Captcha {
+    private int leftOperand;
+    public Captcha (int pettern, int leftOperand, int operator, int rightOperand) {
+        this.leftOperand = leftOperand;
+    }
+    public String getLeft() {
+        return String.valueOf(this.leftOperand);
+    }
 }
--- src/test/java/com/space/learning/unittest/CaptchaTest.java
+++ src/test/java/com/space/learning/unittest/CaptchaTest.java
@@ -1,2 +1,29 @@
-package com.space.learning.unittest;public class CaptchaTest {
+package com.space.learning.unittest;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CaptchaTest {
+    @Order(1)
+    @Test
+    public void leftOperandShouldBe_1() {
+        Captcha captcha = new Captcha(1,1,1,1);
+        Assertions.assertEquals("1", captcha.getLeft());
+    }
+
+    @Order(2)
+    @Test
+    public void leftOperandShouldBe_5() {
+        Captcha captcha = new Captcha(1,5,1,1);
+        Assertions.assertEquals("5", captcha.getLeft());
+    }
+
+    @Order(3)
+    @Test
+    public void leftOperandShouldBe_9() {
+        Captcha captcha = new Captcha(1,9,1,1);
+        Assertions.assertEquals("9", captcha.getLeft());
+    }
 }
